e965ed297381fac01144d9a39c572fd6daf485c6
Ollie Beenham <beenhamow@outlook.com>
2023-05-12 13:11:35 +0100

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ChakraProvider>
+            <NextNProgress options={{ showSpinner: false }} color='#ff9000' />
+            <Component {...pageProps} />
+            <ToastContainer />
+        </ChakraProvider>
+    )
 }
